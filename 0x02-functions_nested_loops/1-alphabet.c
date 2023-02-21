1ffb0e2ff9b1efba687871f8fd62d79532f5fc30
Ebrahim Mangera <ebrahimmangera9@gmail.com>
2023-02-21 21:23:45 +0000

    sp fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,10 +8,12 @@
 
 void print_alphabet(void)
 {
-char i;
-for (i = 'a'; i <= 'z'; i++)
-_putchar(i);
-_putchar('\n');
+char letter = 'a';
 
-return (0);
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
 }
