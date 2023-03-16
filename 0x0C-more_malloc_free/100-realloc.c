75fbd91110755e5506691eac6207874b0a21b8d0
Ebrahim <122838783+EbrahimM78@users.noreply.github.com>
2023-03-16 23:46:19 +0200

    100 Realloc fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,38 +11,43 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-unsigned int i;
-char *newarr, *oldptr;
-
-oldptr = ptr;
+void *mem;
+char *ptr_copy, *filler;
+unsigned int index;
 
 if (new_size == old_size)
-{
 return (ptr);
-}
-else if (new_size == 0 && ptr != NULL)
-{
-free(ptr);
-return (NULL);
-}
-newarr = malloc((new_size));
 
-if (newarr == NULL)
-{
-free(ptr);
-return (NULL);
-}
 if (ptr == NULL)
 {
-free(ptr);
-return ((void *)newarr);
+mem = malloc(new_size);
+
+if (mem == NULL)
+return (NULL);
+
+return (mem);
 }
-for (i = 0; i < old_size; i++)
+
+if (new_size == 0 && ptr != NULL)
 {
-newarr[i] = oldptr[i];
-if (new_size == i)
-break;
-}
 free(ptr);
-return ((void *)newarr);
+return (NULL);
+}
+
+ptr_copy = ptr;
+mem = malloc(sizeof(*ptr_copy) * new_size);
+
+if (mem == NULL)
+{
+free(ptr);
+return (NULL);
+}
+
+filler = mem;
+
+for (index = 0; index < old_size && index < new_size; index++)
+filler[index] = *ptr_copy++;
+
+free(ptr);
+return (mem);
 }
