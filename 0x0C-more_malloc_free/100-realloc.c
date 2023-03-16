1703ad2b94fa95c933e508cd253763108060c364
Ebrahim <122838783+EbrahimM78@users.noreply.github.com>
2023-03-16 23:40:19 +0200

    100 realloc fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,39 +11,38 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *ptr1;
-unsigned int x;
+unsigned int i;
+char *newarr, *oldptr;
 
-if (!ptr)
-{
-ptr1 = malloc(new_size);
-if (!ptr1)
-return (NULL);
-return (ptr1);
-}
-
-if (new_size == 0 && ptr != NULL)
-{
-free(ptr);
-return (NULL);
-}
+oldptr = ptr;
 
 if (new_size == old_size)
+{
 return (ptr);
-
-ptr1 = malloc(new_size);
-
-if (!ptr1)
-return (NULL);
-
-if (new_size < old_size)
-for (; x < new_size; x++)
-((char *)ptr1)[x] = ((char *)ptr)[x];
-else
-for (; x < old_size; x++)
-((char *)ptr1)[x] = ((char *)ptr)[x];
-
+}
+else if (new_size == 0 && ptr != NULL)
+{
 free(ptr);
+return (NULL);
+}
+newarr = malloc((new_size));
 
-return (ptr1);
+if (newarr == NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL)
+{
+free(ptr);
+return ((void *)newarr);
+}
+for (i = 0; i < old_size; i++)
+{
+newarr[i] = oldptr[i];
+if (new_size == i)
+break;
+}
+free(ptr);
+return ((void *)newarr);
 }
