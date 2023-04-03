05318d8138a216ee5d199fb5955ad43adeab34ec
Ebrahim Mangera <ebrahimmangera9@gmail.com>
2023-04-03 23:22:53 +0000

    additional task fix

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,28 +8,27 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-listint_t *slower, *faster;
+listint_t *slow = head, *fast = head;
 
-while (head && (*head).next)
-{
-slower = head;
-faster = (*head).next;
-while (slower != faster)
-{
-if (slower)
-slower = (*slower).next;
-if (faster == head)
-return (head);
-if (faster)
-faster = (*faster).next;
-if (faster == head)
-return (head);
-if (faster)
-faster = (*faster).next;
-if (faster == head)
-return (head);
-}
-head = (*head).next;
-}
+if (head == NULL || head->next == NULL)
 return (NULL);
+slow = slow->next;
+fast = fast->next->next;
+while (fast && fast->next)
+{
+if (slow == fast)
+break;
+slow = slow->next;
+fast = fast->next->next;
+}
+if (slow != fast)
+return (NULL);
+
+slow = head;
+while (slow != fast)
+{
+slow = slow->next;
+fast = fast->next;
+}
+return (slow);
 }
