89f6716c415616df6dafed9bf160d0589fbbf178
Ebrahim Mangera <ebrahimmangera9@gmail.com>
2023-04-11 00:29:30 +0000

    5 fix

--- 0x14-bit_manipulation/5-flip_bit.c
+++ 0x14-bit_manipulation/5-flip_bit.c
@@ -11,12 +11,12 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned long int xor = n ^ m, bit = 0;
+unsigned long int xor = n ^ m, bits = 0;
 
 while (xor > 0)
 {
-bit += (xor & 1);
+bits += (xor & 1);
 xor >>= 1;
 }
-return (bit);
+return (bits);
 }
