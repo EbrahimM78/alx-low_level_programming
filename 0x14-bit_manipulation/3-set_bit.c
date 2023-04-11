f0681230141df9fbf4063f06e5b67ece650357eb
Ebrahim Mangera <ebrahimmangera9@gmail.com>
2023-04-11 00:11:40 +0000

    3 fix

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,10 +9,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= 63 )
+unsigned int a;
+
+if (index > 63)
 return (-1);
 
-*n ^= (1 << index);
+a = 1 << index;
+*n = (*n | m);
 
 return (1);
 }
