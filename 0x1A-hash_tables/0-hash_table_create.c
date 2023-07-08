d2a95648bf739ec9f3d6936e990dc4f1101a4968
Ebrahim Mangera <ebrahimmangera9@gmail.com>
2023-07-08 01:29:35 +0000

    Fixed 0 Removed trailing white space

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,34 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - A function that creates a hash table
- * @size: The size of the array
- *
- * Return: If an error occurs - NULL
- *         Otherwise - a pointer to the new hash table
- */
+* hash_table_create - Creates a hash table.
+* @size: The size of the array.
+*
+* Return: If an error occurs - NULL.
+*         Otherwise - a pointer to the new hash table.
+*/
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table_created;
+	hash_table_t *ta = malloc(sizeof(hash_table_t));
 	unsigned long int i;
 
-	/* allocate space for the hash table */
-	hash_table_created = malloc(sizeof(hash_table_t));
-
-	if (hash_table_created == NULL)
-		return (NULL);
-
-	hash_table_created->size = size;
-	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
-
-	if (hash_table_created->array == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
+	if (ta == NULL)
 	{
-		hash_table_created->array[i] = NULL;
+		return (NULL);
 	}
 
-	return (hash_table_created);
+	ta->size = size;
+	ta->array = malloc(sizeof(hash_node_t *) * size);
+	if (ta->array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		ta->array[i] = NULL;
+
+	return (ta);
 }
+
